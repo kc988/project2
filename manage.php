a2af8dd612cfbdcc47d68dca44e9de4780dc1a59
moniratanak1 <105708053@student.swin.edu.au>
2026-05-22 22:57:09 +1000

    managee

--- manage.php
+++ manage.php
@@ -1,4 +1,37 @@
 <?php
+/* ----------------------------------------------------------------
+   auth_check.php
+   Include this file at the top of manage.php to protect it:
+       <?php require_once 'auth_check.php'; ?>
+---------------------------------------------------------------- */
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params([
+        'lifetime' => 0,
+        'path'     => '/',
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+    session_start();
+}
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$timeout_duration = 1800;
+if (isset($_SESSION['login_time'])
+    && (time() - $_SESSION['login_time']) > $timeout_duration) {
+    $_SESSION = [];
+    session_destroy();
+    header('Location: login.php?timeout=1');
+    exit;
+}
+
+$_SESSION['login_time'] = time();
+?>
+<?php
 session_set_cookie_params([
     'lifetime' => 0,
     'path'     => '/',
